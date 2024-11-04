0823b4977fecca57046e69076b1b43d80ee9be5a
Gmera <gonzalo.mera@estudiantat.upc.edu>
2024-11-04 18:40:11 +0100

    Adding serializer compatibility

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from .models import posts
+from .serilizers import postSerializer
 
 # Create your views here.
 
