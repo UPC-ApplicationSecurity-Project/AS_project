9a4aa3f61e3d05e2ba75fc156d770332d6a7ffc9
fmejfre <jorge.felipe.mejia@estudiantat.upc.edu>
2024-11-09 12:43:58 +0100

    Create server.js
    SSL selg signed certificate

---
+++ frontend/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const PORT = 443;
+
+// Set up SSL certificates
+const sslOptions = {
+  key: fs.readFileSync('/etc/ssl/app-sec/app-sec.key'),       // Path to your private key
+  cert: fs.readFileSync('/etc/ssl/app-sec/app-sec.crt'),    // Path to your full chain certificate
+};
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Redirect all other requests to the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// Start the HTTPS server
+https.createServer(sslOptions, app).listen(PORT, () => {
+  console.log(`Server is running on https://localhost:${PORT}`);
+});
+
+// Create an HTTP server to redirect to HTTPS
+http.createServer((req, res) => {
+  // Redirect all incoming requests to HTTPS
+  res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
+  res.end();
+}).listen(80, () => {
+  console.log('HTTP Server is redirecting all traffic to HTTPS on port 443');
+});
+
