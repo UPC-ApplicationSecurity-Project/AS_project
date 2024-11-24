fdd8d5298680aabe8f136413acc9d1ce37605ed5
fmejfre <jorge.felipe.mejia@estudiantat.upc.edu>
2024-11-24 20:18:51 +0100

    Certificado auto-firmado

--- frontend/project_vite/vite.config.js
+++ frontend/project_vite/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync('./certs/app-sec.key'),  // cargar certificado
+      cert: fs.readFileSync('./certs/app-sec.crt'), // cargar llave
+    },
+    host: '0.0.0.0', // Opcional: para permitir conexiones desde otras máquinas
+    port: 443,      // Ajusta el puerto según prefieras
+  },
 })
+
