f1fbd529b49fb5917cf5d042190ed9b15f3b631f
Gmera <gonzalo.mera@estudiantat.upc.edu>
2024-11-26 22:57:54 +0100

    Adding post support

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -16,6 +16,20 @@ class postView(viewsets.ModelViewSet):
     serializer_class = postSerializer
     queryset = post.objects.all()
 
+    def create(self,request):
+        #user = request.user.pk
+        new_post = post()#request.body.decode('utf-8'))
+        new_post.title = request.POST.get('title')
+        new_post.body = request.POST.get('body')
+        new_post.status = request.POST.get('status')
+        new_post.user = NewUser.objects.get(pk=request.user.pk)
+        new_post.full_clean()
+        new_post.save()
+        return Response("Post created")
+
+    def update(self,request):
+        return Response(None)
+
 class NewUserView(viewsets.ModelViewSet):
     serializer_class = NewUserSerializer
     queryset = NewUser.objects.all()
