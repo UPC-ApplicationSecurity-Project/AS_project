1427d80d93efc18465f518d14b2760cadb6d9b9d
fmejfre <jorge.felipe.mejia@estudiantat.upc.edu>
2024-11-26 11:50:35 +0100

    mejora en interfaz Noticias.jsx

--- frontend/project_vite/src/Pages/Noticias.jsx
+++ frontend/project_vite/src/Pages/Noticias.jsx
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import { PostList } from '../Components/PostList';
 import { Navbar } from '../Components/Navbar';
+import './Noticias.css';  // Importa el archivo de estilos
 
-//PÁGINA QUE MUESTRA LAS NOTICIAS
-
-function Noticias(){
-  
-   return (
-// Muestra barra de navegación
-  <><Navbar />
-{/*Muestra lista de Noticias*/} 
-  <PostList /></>)
-
+// PÁGINA QUE MUESTRA LAS NOTICIAS
+function Noticias() {
+  return (
+    <>
+      <Navbar />
+      {/* Contenedor principal para las noticias */}
+      <div className="news-container">
+        <h1 style={{ textAlign: 'center', color: '#333' }}>Noticias Recientes</h1>
+        {/* Muestra la lista de noticias */}
+        <PostList />
+      </div>
+    </>
+  );
 }
-
 
 export default Noticias;
