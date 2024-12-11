993e5b072bb6766a74def6655ae8a3350b1ba662
Gmera <gonzalo.mera@estudiantat.upc.edu>
2024-12-11 19:32:47 +0100

    Adding encryption to database

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, User
 from django.contrib.auth.hashers import make_password
 from django.conf import settings
+from tink_fields import EncryptedCharField,EncryptedTextField
+
 
 # Create your models here.
     
@@ -10,8 +12,12 @@ class NewUser(User):
         ('Admin', 'Admin'),
         ('Usuario', 'Usuario'),
     ]
+    User.username = EncryptedCharField(max_length=150)
+    User.first_name = EncryptedCharField(max_length=150)
+    User.last_name = EncryptedCharField(max_length=150)
+    User.email = EncryptedCharField(max_length=150)
     created_at = models.DateTimeField(auto_now_add=True)
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='Usuario')
+    role = EncryptedCharField(max_length=10, choices=ROLE_CHOICES, default='Usuario')
 
     def save(self, *args, **kwargs):
         # Hash the password only if it’s not already hashed
@@ -27,9 +33,9 @@ class NewUser(User):
 class post(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, related_name="posts", null=True)
-    title = models.CharField(max_length=30)
-    body = models.TextField(blank = True)
-    link = models.TextField(null=True, max_length=200)
+    title = EncryptedCharField(max_length=30)
+    body = EncryptedTextField(blank = True)
+    link = EncryptedTextField(null=True, max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
          return self.title
