ea7d630b985f210906278456c57d8e2937faaefe
fmejfre <jorge.felipe.mejia@estudiantat.upc.edu>
2024-12-11 21:53:37 +0100

    certlocalhost

--- frontend/project_vite/vite.config.js
+++ frontend/project_vite/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     https: {
-      key: fs.readFileSync('./app-sec-key.pem'),  // cargar certificado
-      cert: fs.readFileSync('./app-sec.pem'), // cargar llave
+      key: fs.readFileSync('./localhost_key.pem'),  // cargar certificado
+      cert: fs.readFileSync('./localhost_crt.pem'), // cargar llave
     },
     host: '0.0.0.0', // Opcional: para permitir conexiones desde otras máquinas
     port: 443,      // Ajusta el puerto según prefieras
